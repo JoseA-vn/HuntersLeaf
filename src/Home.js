631afb37d1bb17442aea424c0d1d8b3c525ce17d
José Vera <jose.vera@alumnos.uv.cl>
2019-10-04 19:20:12 -0300

    Habilitación de un login.

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Grid, Message, Segment } from 'semantic-ui-react';
 import Menu from './Menu';
 import Background from "./Images/BlackWood.jpg";
+import Login from "./Login";
 
 var sectionBackground = {
   width: "100%",
-  height: "640px",
+  height: "100vh",
   backgroundImage: `url(${Background})`
 
 }
@@ -13,21 +14,25 @@ var sectionBackground = {
 export default () => (
   <div>
     <section style={sectionBackground}>
-    <Menu />
-    <Grid centered columns={2}>
-      <Grid.Row>
-      </Grid.Row>
-      <Grid.Row>
-      </Grid.Row>
-      <Segment basic>
-        <Grid centered columns={3}>
-          <Message size="mini">
-            Esta es la página principal de este módulo, su objetivo aún no es claro y sólo debiese servir
-            para redireccionar hacia el resto de páginas.
+      <Menu />
+      <Grid centered columns={2}>
+        <Grid.Row>
+        </Grid.Row>
+        <Grid.Row>
+        </Grid.Row>
+        <Segment basic>
+          <Grid centered columns={3}>
+            <Message size="mini">
+              Esta es la página principal de este módulo, su objetivo aún no es claro y sólo debiese servir
+              para redireccionar hacia el resto de páginas.
         </Message>
-        </Grid>
-      </Segment>
-    </Grid>
+          </Grid>
+        </Segment>
+        <Grid.Row>
+
+          <Login />
+        </Grid.Row>
+      </Grid>
     </section>
   </div>
 );
--- src/Login.js
+++ src/Login.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Button,
   Form,
-  Grid,
   Message,
   Segment,
 } from 'semantic-ui-react';
-import AnimatedBg from "react-animated-bg";
 /*Base de un Login para testeo.*/
-export default () => (
-  
-  <Grid centered columns={3}>
-    <Grid.Column>
-    <Grid centered columns={2}>
-      <AnimatedBg colors={["#EFF166" , "#D68C11 ","#FF0000 "]} duration={3} delay={0} timingFunction="ease-out" className="section-styles">
-        Página de José
-      </AnimatedBg>
-    </Grid>
-      <Segment>
-        <Form size="mini">
+class FormExampleCaptureValues extends Component {
+  state = { name: '', email: '' }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    const { name, email } = this.state
+    switch(name) {
+      case "jose":
+        if (email == "jose") {
+          window.location.href = "/Jose/"; 
+        }
+        break;
+    }
+  }
+  render() {
+    const { name, email } = this.state
+    return (
+      < Segment >
+        <Form onSubmit={this.handleSubmit} size="mini">
           <Form.Input
             fluid
             icon="user"
             iconPosition="left"
             placeholder="Email address"
+            name='name'
+            value={name}
+            onChange={this.handleChange}
           />
           <Form.Input
             fluid
@@ -31,15 +40,18 @@ export default () => (
             iconPosition="left"
             placeholder="Password"
             type="password"
+            name='email'
+            value={email}
+            onChange={this.handleChange}
           />
-          <Button color="facebook" fluid size="mini" >
-            Conectarse
-          </Button>
+          <Form.Button color="facebook" fluid size="mini" content="Conectarse" />
+
         </Form>
-      </Segment>
-      <Message size="mini">
-        ¿Aún no te has registrado? <a href="#">Crear una Cuenta</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-);
+        <Message size="mini">
+          ¿Aún no te has registrado? <a href="#">Crear una Cuenta</a>
+        </Message>
+      </Segment >
+    )
+  }
+}
+export default FormExampleCaptureValues
